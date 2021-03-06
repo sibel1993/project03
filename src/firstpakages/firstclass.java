c044181465e4bc84db45e68cc629560e2e55f311
Sibel Atilgan <s.atilgan1993@gmail.com>
2021-03-05 23:43:57 -0600

    forth commit from local master

--- src/firstpakages/firstclass.java
+++ src/firstpakages/firstclass.java
@@ -9,7 +9,7 @@ public class firstclass {
 		System.out.println("first commit from remoote master branch");
 		System.out.println("second commit from remoote master branch");
 		System.out.println("third commit from remoote master branch");
-
+        System.out.println("here is a merge conflict i create from local master");
 	}
 
 }
